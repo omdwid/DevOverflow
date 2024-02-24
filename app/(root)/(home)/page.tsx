11ac907a0e82dfe73bb3727cd7ebddbcd8ccc592
omdwid <dwivediomacc2@gmail.com>
2024-02-24 13:08:47 +0530

    Remove commented out code and use the result from getQuestions

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -6,151 +6,150 @@ import NoResult from "@/components/shared/NoResult";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import { getQuestions } from "@/lib/actions/question.action";
-import { get } from "http";
 
 import Link from "next/link";
 import React from "react";
 
-const questions = [
-  {
-    _id: "1",
-    title: "How to optimize Python code for better performance?",
-    tags: [
-      {
-        _id: "1",
-        name: "python",
-      },
-      {
-        _id: "2",
-        name: "sql",
-      },
-    ],
-    author: {
-      _id: "1",
-      name: "John Doe",
-      picture: "url_to_picture",
-    },
-    upvotes: 10,
-    views: 500000,
-    answers: [],
-    createdAt: new Date("2024-01-01T12:00:00.000Z"),
-  },
-  {
-    _id: "2",
-    title: "How to implement advanced SQL queries?",
-    tags: [
-      {
-        _id: "1",
-        name: "python",
-      },
-      {
-        _id: "2",
-        name: "sql",
-      },
-    ],
-    author: {
-      _id: "2",
-      name: "Jane Doe",
-      picture: "url_to_picture",
-    },
-    upvotes: 10,
-    views: 100,
-    answers: [],
-    createdAt: new Date("2021-09-01T12:00:00.000Z"),
-  },
-  {
-    _id: "3",
-    title: "Best practices for building responsive React components",
-    tags: [
-      {
-        _id: "3",
-        name: "javascript",
-      },
-      {
-        _id: "4",
-        name: "react",
-      },
-    ],
-    author: {
-      _id: "3",
-      name: "Alice Smith",
-      picture: "url_to_picture",
-    },
-    upvotes: 15,
-    views: 120,
-    answers: [],
-    createdAt: new Date("2021-09-02T12:00:00.000Z"),
-  },
-  {
-    _id: "4",
-    title: "Introduction to Spring framework for Java developers",
-    tags: [
-      {
-        _id: "5",
-        name: "java",
-      },
-      {
-        _id: "6",
-        name: "spring",
-      },
-    ],
-    author: {
-      _id: "4",
-      name: "Bob Johnson",
-      picture: "url_to_picture",
-    },
-    upvotes: 8,
-    views: 90,
-    answers: [],
-    createdAt: new Date("2021-09-03T12:00:00.000Z"),
-  },
-  {
-    _id: "5",
-    title: "Getting started with Angular and TypeScript",
-    tags: [
-      {
-        _id: "7",
-        name: "typescript",
-      },
-      {
-        _id: "8",
-        name: "angular",
-      },
-    ],
-    author: {
-      _id: "5",
-      name: "Eva Martinez",
-      picture: "url_to_picture",
-    },
-    upvotes: 20,
-    views: 150,
-    answers: [],
-    createdAt: new Date("2021-09-04T12:00:00.000Z"),
-  },
-  {
-    _id: "6",
-    title: "Building web applications with Ruby on Rails",
-    tags: [
-      {
-        _id: "9",
-        name: "ruby",
-      },
-      {
-        _id: "10",
-        name: "rails",
-      },
-    ],
-    author: {
-      _id: "6",
-      name: "Michael Brown",
-      picture: "url_to_picture",
-    },
-    upvotes: 12,
-    views: 110,
-    answers: [],
-    createdAt: new Date("2021-09-05T12:00:00.000Z"),
-  },
-];
+// const questions = [
+//   {
+//     _id: "1",
+//     title: "How to optimize Python code for better performance?",
+//     tags: [
+//       {
+//         _id: "1",
+//         name: "python",
+//       },
+//       {
+//         _id: "2",
+//         name: "sql",
+//       },
+//     ],
+//     author: {
+//       _id: "1",
+//       name: "John Doe",
+//       picture: "url_to_picture",
+//     },
+//     upvotes: 10,
+//     views: 500000,
+//     answers: [],
+//     createdAt: new Date("2024-01-01T12:00:00.000Z"),
+//   },
+//   {
+//     _id: "2",
+//     title: "How to implement advanced SQL queries?",
+//     tags: [
+//       {
+//         _id: "1",
+//         name: "python",
+//       },
+//       {
+//         _id: "2",
+//         name: "sql",
+//       },
+//     ],
+//     author: {
+//       _id: "2",
+//       name: "Jane Doe",
+//       picture: "url_to_picture",
+//     },
+//     upvotes: 10,
+//     views: 100,
+//     answers: [],
+//     createdAt: new Date("2021-09-01T12:00:00.000Z"),
+//   },
+//   {
+//     _id: "3",
+//     title: "Best practices for building responsive React components",
+//     tags: [
+//       {
+//         _id: "3",
+//         name: "javascript",
+//       },
+//       {
+//         _id: "4",
+//         name: "react",
+//       },
+//     ],
+//     author: {
+//       _id: "3",
+//       name: "Alice Smith",
+//       picture: "url_to_picture",
+//     },
+//     upvotes: 15,
+//     views: 120,
+//     answers: [],
+//     createdAt: new Date("2021-09-02T12:00:00.000Z"),
+//   },
+//   {
+//     _id: "4",
+//     title: "Introduction to Spring framework for Java developers",
+//     tags: [
+//       {
+//         _id: "5",
+//         name: "java",
+//       },
+//       {
+//         _id: "6",
+//         name: "spring",
+//       },
+//     ],
+//     author: {
+//       _id: "4",
+//       name: "Bob Johnson",
+//       picture: "url_to_picture",
+//     },
+//     upvotes: 8,
+//     views: 90,
+//     answers: [],
+//     createdAt: new Date("2021-09-03T12:00:00.000Z"),
+//   },
+//   {
+//     _id: "5",
+//     title: "Getting started with Angular and TypeScript",
+//     tags: [
+//       {
+//         _id: "7",
+//         name: "typescript",
+//       },
+//       {
+//         _id: "8",
+//         name: "angular",
+//       },
+//     ],
+//     author: {
+//       _id: "5",
+//       name: "Eva Martinez",
+//       picture: "url_to_picture",
+//     },
+//     upvotes: 20,
+//     views: 150,
+//     answers: [],
+//     createdAt: new Date("2021-09-04T12:00:00.000Z"),
+//   },
+//   {
+//     _id: "6",
+//     title: "Building web applications with Ruby on Rails",
+//     tags: [
+//       {
+//         _id: "9",
+//         name: "ruby",
+//       },
+//       {
+//         _id: "10",
+//         name: "rails",
+//       },
+//     ],
+//     author: {
+//       _id: "6",
+//       name: "Michael Brown",
+//       picture: "url_to_picture",
+//     },
+//     upvotes: 12,
+//     views: 110,
+//     answers: [],
+//     createdAt: new Date("2021-09-05T12:00:00.000Z"),
+//   },
+// ];
 
 const page = async () => {
   const result = await getQuestions({});
@@ -183,8 +182,8 @@ const page = async () => {
       <HomeFilter />
 
       <div className="mt-10 flex w-full flex-col gap-6">
-        {questions.length > 0 ? (
-          questions.map((question) => (
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
