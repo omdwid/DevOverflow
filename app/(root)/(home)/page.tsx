26ab08c5a062c6750c96f9b469117bc38495a83f
omdwid <dwivediomacc2@gmail.com>
2024-03-02 21:33:36 +0530

    Fix type annotations in map functions

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -183,7 +183,7 @@ const page = async () => {
 
       <div className="mt-10 flex w-full flex-col gap-6">
         {result.questions.length > 0 ? (
-          result.questions.map((question) => (
+          result.questions.map((question: any) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
